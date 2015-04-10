6920992b30e46438227840e2d2e9b683cf24e33a
Zhenya <eugeneas@gmail.com>
2015-04-10 17:55:28 -0400

    tweaks to package.js

--- package.js
+++ package.js
@@ -1,11 +1,8 @@
 Package.describe({
   name: 'zhenya:boilerplate',
   version: '0.0.1',
-  summary: 'Less Coffee boilerplate',
-  // URL to the Git repository containing the source code for this package.
+  summary: 'Less Coffee boilerplate: less imports, utils and helpers',
   git: 'https://github.com/zhenyasav/meteor-boilerplate',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
